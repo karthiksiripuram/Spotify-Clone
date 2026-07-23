4fb154762763ce36e748ca31c02bc7d601320a60
karthiksiripuram <karthiksiripuram92@gmail.com>
2026-07-23 23:12:00 +0530

    Fix fetch paths for Vercel

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbum() {
-    let a = await fetch(`http://127.0.0.1:5500/songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -85,7 +85,7 @@ async function displayAlbum() {
         if (e.href.includes("/songs/")) {
             let folder = (e.href.split("/").slice(-1)[0]);
             //Get the meta data of the folder
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json();
 
 
