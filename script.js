1624783dae0cc00d98cebd2620ed0d8f5f7adfe8
karthiksiripuram <karthiksiripuram92@gmail.com>
2026-07-24 22:07:00 +0530

    Fix Vercel deployment

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function formatTime(seconds) {
 
 async function getSongs(folder) {
     CurrFolder = folder;
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
