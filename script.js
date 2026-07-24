da9703a86d3bfd81aeb29bcb7179d7303ac45887
karthiksiripuram <karthiksiripuram92@gmail.com>
2026-07-24 22:39:33 +0530

    Use songs.json instead of directory listing

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ document.addEventListener("click", (e) => {
 async function main() {
 
     //Get list of all songs
-    await getSongs("songs/Hindi")
+    await getSongs("/songs/")
     playMusic(songs[0], true)
 
     //Display all the albums on the page
