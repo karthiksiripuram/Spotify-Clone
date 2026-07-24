23db240be60b2babbeb7aaa0c641b79e9e02f5c1
karthiksiripuram <karthiksiripuram92@gmail.com>
2026-07-24 22:31:28 +0530

    Use songs.json instead of directory listing

--- script.js
+++ script.js
@@ -19,45 +19,42 @@ function formatTime(seconds) {
 }
 
 async function getSongs(folder) {
+
     CurrFolder = folder;
-    let a = await fetch(`/${folder}/`)
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a")
-    songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/${folder}/`)[1])
-        }
 
-    }
+    let res = await fetch(`/${folder}/songs.json`);
+    let data = await res.json();
 
-    //Show all songs in the playlist
-    let songUL = document.querySelector(".songsList").getElementsByTagName("ul")[0]
-    songUL.innerHTML = ""
+    songs = data.songs;
+
+    let songUL = document.querySelector(".songsList ul");
+    songUL.innerHTML = "";
+
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li> 
-                    <img class="invert" src="img/music.svg" alt="">
-                    <div class="info">
-                        <div>${song.replaceAll("%20", " ")}</div>
-                        <div>Artist Name</div>
-                    </div>
-                    <div class="playnow">
-                        <span>Play now</span>
-                        <img class="invert" src="play.svg" alt="">
-                    </div></li>`;
+
+        songUL.innerHTML += `
+        <li>
+            <img class="invert" src="img/music.svg" alt="">
+            <div class="info">
+                <div>${song.replace(".mp3","")}</div>
+                <div>Artist Name</div>
+            </div>
+            <div class="playnow">
+                <span>Play now</span>
+                <img class="invert" src="play.svg" alt="">
+            </div>
+        </li>`;
     }
 
-    //Attach Eventlistener to each song
-    Array.from(document.querySelector(".songsList").getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click", element => {
-            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+    Array.from(songUL.getElementsByTagName("li")).forEach((e,index)=>{
 
-        })
-        console.log(e.querySelector(".info").firstElementChild.innerHTML.trim());
-    })
+        e.addEventListener("click",()=>{
+
+            playMusic(songs[index]);
+
+        });
+
+    });
 
     return songs;
 }
